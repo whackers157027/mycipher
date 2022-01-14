e3460d9e1f253db6189daf63c51f2c24de6ed4fa
whackers157027 <97718395+whackers157027@users.noreply.github.com>
2022-01-13 20:11:12 -0800

    Create caesar.py
    caesar salad is delicious

---
+++ caesar.py
@@ -0,0 +1,25 @@
+#A python program to illustrate Caesar Cipher Technique from https://www.geeksforgeeks.org/caesar-cipher-in-cryptography/
+def encrypt(text,s):
+	result = ""
+
+	# traverse text
+	for i in range(len(text)):
+		char = text[i]
+
+		# Encrypt uppercase characters
+		if (char.isupper()):
+			result += chr((ord(char) + s-65) % 26 + 65)
+
+		# Encrypt lowercase characters
+		else:
+			result += chr((ord(char) + s - 97) % 26 + 97)
+
+	return result
+
+#check the above function
+text = "" # flag to be put here
+s = 4
+print "Text : " + text
+print "Shift : " + str(s)
+print "Cipher: " + encrypt(text,s)
+
